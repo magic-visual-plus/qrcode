ca3aa02d0f702c3b292b7a548a5c81d65107bda3
zouxiaochuan <zouxiaochuan@163.com>
2025-03-01 17:25:26 +0800

    update code

--- qrcode/qrcodes.py
+++ qrcode/qrcodes.py
@@ -8,6 +8,14 @@ import os
 import numpy as np
 from scipy import ndimage
 
+def transform_image(image):
+    yield image
+
+    for i in range(3):
+        image[:, :, i] = cv2.equalizeHist(image[:, :, i])
+        yield image
+    pass
+
 
 def detect_qrcode(detector, image):
     detections = detector.detect(image=image, is_bgr=True)
@@ -21,23 +29,31 @@ def detect_qrcode(detector, image):
     # print(detection)
     x1, y1, x2, y2 = detection['bbox_xyxy']
 
-    x1 = int(x1) - 2
-    y1 = int(y1) - 2
-    x2 = int(x2) + 2 
-    y2 = int(y2) + 2
+    x1 = int(x1)
+    y1 = int(y1)
+    x2 = int(x2)
+    y2 = int(y2)
 
-    img = image[y1:y2, x1:x2]
-    img = cv2.GaussianBlur(img, (3, 3), 0)
+    x11 = x1 - 2
+    y11 = y1 - 2
+    x22 = x2 + 2
+    y22 = y2 + 2
 
-    for d in range(-180, 180, 30):
-        img_ = ndimage.rotate(img, d)
-        qr_code = pyzbar.pyzbar.decode(Image.fromarray(img_))
+    img = image[y11:y22, x11:x22]
+    # normalize contrast
 
-        if not qr_code:
-            # print(f'QR code: {qr_code[0].data.decode("utf-8")}')
-            continue
-        else:
-            return qr_code[0].data.decode('utf-8')
+    for img in transform_image(img):
+        img = cv2.GaussianBlur(img, (3, 3), 0)
+        for d in range(-180, 180, 30):
+            img_ = ndimage.rotate(img, d)
+            qr_code = pyzbar.pyzbar.decode(Image.fromarray(img_))
+
+            if not qr_code:
+                # print(f'QR code: {qr_code[0].data.decode("utf-8")}')
+                continue
+            else:
+                return qr_code[0].data.decode('utf-8')
+            pass
         pass
 
     return None
